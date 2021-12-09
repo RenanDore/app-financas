2b1f4eff277dac523244832b04268b67eac1e67a
Renan Dore <dore.renan@gmail.com>
2021-12-09 15:37:12 -0300

    Criando styled components e as imagens da tela principal

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,53 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { Platform } from "react-native";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Background,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText,
+} from "./styles";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <Background>
+      <Container behavior={Platform.OS === "ios" ? "padding" : "height"}>
+        <Logo source={require("../../assets/Logo.png")} />
+        <AreaInput>
+          <Input
+            placeholder="Email"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+
+          <Input
+            placeholder="Senha"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link>
+          <LinkText>Criar uma conta!</LinkText>
+        </Link>
+      </Container>
+    </Background>
   );
 };
 
